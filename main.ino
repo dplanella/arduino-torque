632960a3a1a903e0a7c76e4c29ba80499736085d
David Planella <david.planella@gmail.com>
2017-11-24 23:51:06 +0100

    Minor changes: don't use magic numbers

--- main.ino
+++ main.ino
@@ -1,5 +1,4 @@
 #include <SoftwareSerial.h>
-#include <String.h>
  
 /**
  * Torque for Android Arduino sample program by Ian Hawkins <ian@torque-bhp.com> http://torque-bhp.com/
@@ -10,7 +9,9 @@
  * 
  * This code is released under the LGPL v2 
  * 
- * This has been tested on the Arduino UNO board
+ * This has been tested on:
+ * - Arduino UNO
+ * - Arduino Nano
  * 
  * Connect pins 2 and 3 to your bluetooth device's rx and tx pins (2 to txd on bluetooth device, 3 to rxd)
  * 
@@ -42,25 +43,29 @@ const String ANALOG = "a";
 const String DIGITAL = "d";
 const String IS_INPUT = "i";
 const String IS_OUTPUT = "o";
- 
+
+// RX buffer
 String fromTorque = "";
  
 /**
  * Array of sensors we will advertise to Torque so it can automatically import them. Using strings
  * Stucture is:
  *  
- *  Arduino Pin, Arduino pin type, Input/Ouput, Default value(if output), ShortName, Long name, units, minimum value, maximum value
+ *  Arduino Pin, Arduino pin type, Input/Ouput, Default value(if output), ShortName, Long name,
+ *      units, minimum value, maximum value
  *  
  *  Caveats:  Don't use a '>' in any of the names, 
- *            Update 'sensorsSize' with the number of elements.
+ *            Update 'SENSORSCHANNELS' with the number of channels.
  *            Analog outputs are PWM on digital pins.
  *  
  */
-const int SENSORSSIZE = 9 * 3; // each line is 9 attributes, and we have 3 lines.
+const int SENSORSCHANNELS = 3;
+const int SENSORSATTRIBUTES = 9;
+const int SENSORSSIZE = SENSORSATTRIBUTES * SENSORSCHANNELS; // each line is 9 attributes, and we have 3 lines.
 const String sensors[SENSORSSIZE] = {
-                    "0", ANALOG, IS_INPUT,   "-",   "Pot 1", "Potentiometer 1", "v",    "0", "5",
-                    "1", ANALOG, IS_INPUT,   "-",   "LDR 1", "Light Sensor 1",  "v",    "0", "5",
-                    "4", DIGITAL,IS_OUTPUT,  "0",  "Dout1", "Digital Out 1",   "bit", "0", "1"
+                    "0", ANALOG,  IS_INPUT,   "-",  "Pot 1", "Potentiometer 1",  "v", "0", "5",
+                    "1", ANALOG,  IS_INPUT,   "-",  "LDR 1", "Light Sensor 1",   "v", "0", "5",
+                    "4", DIGITAL, IS_OUTPUT,  "0",  "Dout1", "Digital Out 1",  "bit", "0", "1"
                    }; 
 /**
  * Configuration directives for the app to hide various things. Comma separated. Remove to enable visibility in Torque
@@ -74,16 +79,23 @@ const String sensors[SENSORSSIZE] = {
 const String CONFIGURATION = "NO_CAR_SENSORS,NO_DEVICE_SENSORS"; 
  
 // Setup bluetooth module on pins 2 and 3 (you can't use these digial pins in the sensor list or it'll break comms)
-SoftwareSerial mySerial(2,3); // Most other boards can use pins 2 and 3
-//SoftwareSerial mySerial(8,9); // Use pins 8,9 on Arduino Micro
+// Use pins 8, 9 on Arduino Micro
+// Most other boards can use pins 2 and 3
+
+const int rxPin = 2;
+const int txPin = 3;
+
+SoftwareSerial mySerial(rxPin, txPin);
+
+const int BAUDRATE = 9600;
  
  
 void setup() {
   // Init the pins 
   initSensors();
-  Serial.begin(9600);    // the GPRS baud rate 
+  Serial.begin(BAUDRATE);    // the GPRS baud rate 
   delay(600);
-  mySerial.begin(9600);              
+  mySerial.begin(BAUDRATE);              
  }
  
 void loop() {
@@ -92,11 +104,14 @@ void loop() {
    * Grab data from the bluetooth module, parse it.
    */
   if (mySerial.available()) {
-     char c = mySerial.read();   
+     char c = mySerial.read();
+     // If end of command is detected, process the command buffer
+     // and clear it afterwards
      if ((c == '\n' || c == '\r') && fromTorque.length() > 0) {
         fromTorque.toUpperCase();
         processCommand(fromTorque);
         fromTorque = "";
+     // Otherwise, keep reading into the command buffer
      } else if (c != ' ' && c != '\n' && c !='\r') {
         // Ignore spaces.
         fromTorque += c; 
@@ -162,12 +177,12 @@ void processCommand(String command) {
  */
 void showSensorDefinitions() {
    int id = 0;
-   for (int i = 0; i < SENSORSSIZE/9; i++) {
-      for (int j = 0; j < 9; j++) {
-         id = (i*9)+j;
+   for (int i = 0; i < SENSORSCHANNELS; i++) {
+      for (int j = 0; j < SENSORSATTRIBUTES; j++) {
+         id = (i * SENSORSATTRIBUTES) + j;
          mySerial.print(sensors[id]);
  
-         if (id+1 < SENSORSSIZE) {
+         if (id + 1 < SENSORSSIZE) {
             mySerial.print(',');
          }
       }
@@ -181,22 +196,22 @@ void showSensorDefinitions() {
  * Format to Torque is id:type:value
  */
 void getSensorValues() {
-   for (int i = 0; i < SENSORSSIZE/9; i++) {
-      int group = i * 9;
-      int id = sensors[group].toInt();
-      String type = sensors[group+1];
-      boolean isOutput = sensors[group+2].equals(IS_OUTPUT);
+   for (int i = 0; i < SENSORSCHANNELS; i++) {
+      int channel = i * SENSORSATTRIBUTES;
+      int pinNum = sensors[channel].toInt();
+      String pinType = sensors[channel + 1];
+      boolean isOutput = sensors[channel + 2].equals(IS_OUTPUT);
  
- 
+      // Print input value
       if (!isOutput) {
-         mySerial.print(id);
+         mySerial.print(pinNum);
          mySerial.print(":");
-         mySerial.print(type);
+         mySerial.print(pinType);
          mySerial.print(":");
-         if (type.equals(ANALOG)) {
-            mySerial.print(analogRead(id));
-         } else if (type.equals(DIGITAL)) {
-            mySerial.print(digitalRead(id));
+         if (pinType.equals(ANALOG)) {
+            mySerial.print(analogRead(pinNum));
+         } else if (pinType.equals(DIGITAL)) {
+            mySerial.print(digitalRead(pinNum));
          }
          mySerial.print('\n');
       }
@@ -208,22 +223,21 @@ void getSensorValues() {
  */
 void setSensorValue(String command) {
   int index = command.indexOf(":");
-  int id = command.substring(1,index).toInt();
-  int value = command.substring(index+1, command.length()).toInt();
+  int id = command.substring(1, index).toInt();
+  int value = command.substring(index + 1, command.length()).toInt();
  
-  for (int i = 0; i < SENSORSSIZE/9; i++) {
-     int group = i * 9;
-     int sid = sensors[group].toInt();
-     boolean isOutput = sensors[group+2].equals(IS_OUTPUT);
-     if (isOutput) {
- 
-       if (sid == id) {
- 
-          String type = sensors[group+1];
-          if (type.equals(ANALOG)) {
-            analogWrite(sid, constrain(value,0,255));
-          } else if (type.equals(DIGITAL)) {
-            digitalWrite(sid, value > 0 ? HIGH: LOW);
+  for (int i = 0; i < SENSORSCHANNELS; i++) {
+     int channel = i * SENSORSATTRIBUTES;
+     int pinNum = sensors[channel].toInt();
+     boolean isOutput = sensors[channel + 2].equals(IS_OUTPUT);
+
+     if (isOutput) { 
+       if (pinNum == id) {
+          String pinType = sensors[channel + 1];
+          if (pinType.equals(ANALOG)) {
+            analogWrite(pinNum, constrain(value, 0, 255));
+          } else if (pinType.equals(DIGITAL)) {
+            digitalWrite(pinNum, value > 0 ? HIGH: LOW);
           }
           break;
        }
@@ -235,21 +249,20 @@ void setSensorValue(String command) {
  *  Init the sensor definitions (input/output, default output states, etc)
  */
 void initSensors() {
-   for (int i = 0; i < SENSORSSIZE/9; i++) {
-      int group = i * 9;
-      int id = sensors[group].toInt();
-      String type = sensors[group+1];
-      boolean isOutput = sensors[group+2].equals(IS_OUTPUT);
-      int defaultValue = sensors[group+3].toInt();
- 
+   for (int i = 0; i < SENSORSCHANNELS; i++) {
+      int channel = i * SENSORSATTRIBUTES; // Sensor channel index
+      int pinNum = sensors[channel].toInt();
+      String pinType = sensors[channel + 1];
+      boolean isOutput = sensors[channel + 2].equals(IS_OUTPUT);
+      int defaultValue = sensors[channel + 3].toInt();
  
       if (isOutput) {
-         if (type.equals(ANALOG)) {
-             pinMode(id, OUTPUT);
-             analogWrite(id, constrain(defaultValue, 0, 255));
-         } else if (type.equals(DIGITAL)) {
-             pinMode(id, OUTPUT);
-             digitalWrite(id, defaultValue > 0 ? HIGH : LOW);
+         if (pinType.equals(ANALOG)) {
+             pinMode(pinNum, OUTPUT);
+             analogWrite(pinNum, constrain(defaultValue, 0, 255));
+         } else if (pinType.equals(DIGITAL)) {
+             pinMode(pinNum, OUTPUT);
+             digitalWrite(pinNum, defaultValue > 0 ? HIGH : LOW);
          }
       }
    }
